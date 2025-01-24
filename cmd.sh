f4eb976915fd7edc0c7a9fe9abbaa435a197e7b1
maomao.ling <995776046@qq.com>
2025-01-24 08:17:10 +0800

    fix cpu mem leak problem, test tall, alter model,seem not good
    Signed-off-by: maomao.ling <995776046@qq.com>

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,12 @@
+#weight=./training/weights/xception_best.pth
+#weight=./training/weights/altfreezing_best.pth
+weight=./training/weights/tall_trainFF_testCDF.pth
+#config=training/config/detector/altfreezing.yaml
+config=training/config/detector/tall.yaml
+#./training/config/detector/xception.yaml
 set -x
-python3 training/test.py --detector_path ./training/config/detector/xception.yaml --test_dataset  DFDC  --weights_path ./training/weights/xception_best.pth
 
-#python3 training/test.py --detector_path ./training/config/detector/xception.yaml --test_dataset   "FF-DF"  "FF-F2F"  "FF-FS"  "FF-NT" "DeepFakeDetection"  "FaceShifter"  --weights_path ./training/weights/xception_best.pth
-#python3 training/test.py --detector_path ./training/config/detector/xception.yaml --test_dataset   "FF-DF" "FF-FS"  "FF-NT" "DeepFakeDetection"  "FaceShifter"  --weights_path ./training/weights/xception_best.pth
+python3 training/test.py --detector_path $config --test_dataset   "FF-DF"  "FF-F2F"  "FF-FS"  "FF-NT" "DeepFakeDetection"  "FaceShifter"  --weights_path $weight
+
+python3 training/test.py --detector_path $config --test_dataset  DFDC  --weights_path $weight
+#python3 training/test.py --detector_path $config --test_dataset   "FF-DF" "FF-FS"  "FF-NT" "DeepFakeDetection"  "FaceShifter"  --weights_path ./training/weights/xception_best.pth
--- training/test.py
+++ training/test.py
@@ -109,9 +109,19 @@ def test_one_dataset(model, data_loader):
         predictions = inference(model, data_dict)
         label_lists += list(data_dict['label'].cpu().detach().numpy())
         prediction_lists += list(predictions['prob'].cpu().detach().numpy())
-        feature_lists += list(predictions['feat'].cpu().detach().numpy())
+        #feature_lists += list(predictions['feat'].cpu().detach().numpy())
+        """
+        print(f"Number of features in feature_lists: {len(feature_lists)}")
+
+        # 假设每个元素是一个 NumPy 数组，打印它们的形状
+        for i, features in enumerate(feature_lists[:5]):  # 打印前 5 个元素的信息
+            print(f"Feature {i} shape: {features.shape}")
+            break;
+        """
+
     
-    return np.array(prediction_lists), np.array(label_lists),np.array(feature_lists)
+    return np.array(prediction_lists), np.array(label_lists)
+    #return np.array(prediction_lists), np.array(label_lists),np.array(feature_lists)
     
 def test_epoch(model, test_data_loaders):
     # set model to eval mode
@@ -125,7 +135,8 @@ def test_epoch(model, test_data_loaders):
     for key in keys:
         data_dict = test_data_loaders[key].dataset.data_dict
         # compute loss for each dataset
-        predictions_nps, label_nps,feat_nps = test_one_dataset(model, test_data_loaders[key])
+        #predictions_nps, label_nps,feat_nps = test_one_dataset(model, test_data_loaders[key])
+        predictions_nps, label_nps = test_one_dataset(model, test_data_loaders[key])
         
         # compute metric for each dataset
         metric_one_dataset = get_test_metrics(y_pred=predictions_nps, y_true=label_nps,
